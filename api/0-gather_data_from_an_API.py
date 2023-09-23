5cff6728fd2f42af72d04610be1caea5a02ac0e8
Denzel18rb <74129875rb@gmail.com>
2023-09-23 12:35:34 -0700

    modifies names within our lines of code for correct operation

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,13 +5,13 @@ import sys
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com"
-    alls = requests.get(url + "/alls", params={"userId": sys.argv[1]}).json()
     user = requests.get(url + "/users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "/todos", params={"userId": sys.argv[1]}).json()
 
-    Tasuku_no_gokei = len(alls)
-    Kanryo_shita_tasuku = sum(1 for all in alls if all["Kanryo shimashita"])
-    
+    Tasuku_no_gokei = len(todos)
+    Kanryo_shita_tasuku = sum(1 for todo in todos if todo["completed"])
+
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), Kanryo_shita_tasuku, Tasuku_no_gokei))
-    
-    [print(f"\t {all['title']}") for all in alls if all["Kanryo shimashita"]]
+            user.get("name"), Kanryo_shita_tasuku, Tasuku_no_gokei))
+
+    [print(f"\t {todo['title']}") for todo in todos if todo["completed"]]
