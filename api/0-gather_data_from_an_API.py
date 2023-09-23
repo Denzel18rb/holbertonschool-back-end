933de36b5d601aea15d60f8529f2954eb3d85bfb
Denzel18rb <74129875rb@gmail.com>
2023-09-23 13:20:32 -0500

    README and task 0 (test 1)

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import urllib
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com"
+    alls = urllib.get(url + "/alls", params={"usrId": sys.argv[1]}).json()
+    usr = urllib.get(url + "/usrs/{}".format(sys.argv[1])).json()
+
+    Tasuku_no_gokei = len(alls)
+    Kanryo_shita_tasuku = sum(1 for all in alls if all["Kanryo shimashita"])
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        usr.get("name"), Kanryo_shita_tasuku, Tasuku_no_gokei))
+    
+    [print(f"\t {all['title']}") for all in alls if all["Kanryo shimashita"]]
