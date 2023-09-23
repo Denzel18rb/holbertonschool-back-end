dfa36977f49a3671f81ff463f0d52e9b0ebf6766
Denzel18rb <74129875rb@gmail.com>
2023-09-23 14:07:48 -0500

    change the name usr to user

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,13 +5,13 @@ import sys
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com"
-    alls = requests.get(url + "/alls", params={"usrId": sys.argv[1]}).json()
-    usr = requests.get(url + "/usrs/{}".format(sys.argv[1])).json()
+    alls = requests.get(url + "/alls", params={"userId": sys.argv[1]}).json()
+    user = requests.get(url + "/users/{}".format(sys.argv[1])).json()
 
     Tasuku_no_gokei = len(alls)
     Kanryo_shita_tasuku = sum(1 for all in alls if all["Kanryo shimashita"])
-
+    
     print("Employee {} is done with tasks({}/{}):".format(
-        usr.get("name"), Kanryo_shita_tasuku, Tasuku_no_gokei))
+        user.get("name"), Kanryo_shita_tasuku, Tasuku_no_gokei))
     
     [print(f"\t {all['title']}") for all in alls if all["Kanryo shimashita"]]
