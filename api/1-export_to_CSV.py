884c23a3be86c003bc6c07656bd79b4bc149a60f
Denzel18rb <74129875rb@gmail.com>
2023-09-23 14:55:41 -0700

    configure the class names for correct operation

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """ This module defines the REST API """
+import csv
 import requests
 import sys
-import csv
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com"
     user = requests.get(url + "/users/{}".format(sys.argv[1])).json()
-    subete = requests.get(url + "/subete", params={"userId": sys.argv[1]}).json()
+    subete = requests.get(url + "/todos", params={"userId": sys.argv[1]}).json()
     fairu_mei = "{}.csv".format(user["id"])
 
     with open(fairu_mei, mode="w", newline="") as csv_file:
@@ -15,4 +15,4 @@ if __name__ == "__main__":
 
         for todo in subete:
             writer.writerow([user["id"], user["username"],
-                            str(todo["Kanryo_shimashita"]), todo["Taitoru"]])
+                    str(todo["completed"]), todo["title"]])
