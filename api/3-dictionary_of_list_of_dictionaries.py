f0e76191a1863525c2509696d20ccd1c585302b8
Denzel18rb <74129875rb@gmail.com>
2023-09-23 18:39:44 -0500

    Add a file to export data in JSON format.

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" This module defines the REST API """
+import json
+import requests
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com"
+    Benotzer = requests.get(url + "/users").json()
+    subete_modu = {}
+
+    for user in Benotzer:
+        yuza_id = user['id']
+        subete = requests.get(url + "/todos", params={"userId": yuza_id}).json()
+        user_todos = [{"username": user["username"], "task": todo["title"],
+                       "completed": todo["completed"]} for todo in subete]
+        subete_modu[yuza_id] = user_todos
+
+    with open("todo_all_employees.json", "w") as outfile:
+        json.dump(subete_modu, outfile)
